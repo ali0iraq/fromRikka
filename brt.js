5bcce56405af6f7e2a836fb0878928665b021413
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-27 05:45:25 -0700

    Update brt.js

--- brt.js
+++ brt.js
@@ -28,16 +28,6 @@ client.user.setGame(`+help | By Jihad`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-
-client.on('message', message => {
-if(message.content === adminprefix + "restart") {
-      if (!devs.includes(message.author.id)) return;
-          message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
-        console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
-        client.destroy();
-        child_process.fork(__dirname + "/الملف.js");
-        console.log(`تم اعادة تشغيل البوت`);
-    }
     
     client.on('message', message => {
     if (message.content === "+createroles") {
