8edfd55a81195f35d5f09bc1eec07f2614224fe1
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-27 06:50:22 -0700

    Update brt.js

--- brt.js
+++ brt.js
@@ -2,9 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`+help | By Jihad`,"http://twitch.tv/S-F")
+client.on('ready', function(){
+    var ms = 40000 ;
+    var setGame = ['+help','By Jihad'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+j = 1;
+       }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+      }
+       i = i+j;
+        client.user.setGame(setGame[i],http://www.youtube.com/gg);
+}, ms);
+
+});
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
