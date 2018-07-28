478a4e976b85afb0f2ba6ae64cfb2fae571a587f
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-28 11:40:01 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -341,16 +341,6 @@ message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
 }
 });
 
-client.on("message", (message) => {
-if (message.content.startsWith("+cv")) {
-            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
-        let args = message.content.split(" ").slice(1);
-    message.guild.createChannel(args.join(' '), 'voice');
-    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
-    
-}
-});
-
 client.on('message', message => {
     if (message.content.startsWith("+bans")) {
         message.guild.fetchBans()
