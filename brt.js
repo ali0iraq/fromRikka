92629ed458e91f722f562e67e8c0551ab75008da
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-27 20:36:13 -0700

    Update brt.js

--- brt.js
+++ brt.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+'
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -24,6 +26,16 @@ const prefix = '+'
   console.log('')
   console.log('')
 });
+
+client.on('message', msg => {
+  if (msg.content === '+ping') {
+    msg.reply('Po0o0nG');
+  }
+});
+
+
+
+
 
 client.on('ready', function(){
     var ms = 60000 ;
@@ -58,26 +70,27 @@ client.on('message', function(message) {
 });
 
 
-            
-client.on("message", message => {
-    if(message.content.startsWith(prefix + "server")) {
-        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**You Don't Have Permission ❌**");
-        const embed = new Discord.RichEmbed()
-        .setAuthor(message.guild.name, message.guild.iconURL)
-        .setColor("RANDOM")
 
-.addField('**Members 👤 **' , `${message.guild.memberCount}`)
-.addField('**Server Owner 👑**' , `${message.guild.owner.user.username}`)
-.addField(`**Channels :scroll: **`,true)
-.addField(`# chats`, `${message.guild.channels.filter(m => m.type === 'text').size}`)
-.addField( `:loud_sound:`,`${message.guild.channels.filter(m => m.type === 'voice').size}`)
-.addField(`**Roles**:briefcase:`,`${message.guild.roles.size}`)
-.addField(`📆 Created On`, message.guild.createdAt ,true)
-        message.channel.send({embed:embed})
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'server')){
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('**لا تملك الرتية الكافية لإستخدام هذا الأمر**');
+        var server = new Discord.RichEmbed()
+        .setTitle(`:page_with_curl: [**__${message.guild.name}__**] **معلومات عن سيرفر**`)
+        .addField(`:crown: **__Server Owner:__**`, `**➥** [${message.guild.owner}]`)
+        .addField(`:id: **__Server ID:__**`, `**➥** [**__${message.guild.id}__**]`)
+        .addField(`:two_men_holding_hands: **__Members Count:__**`, `**➥** [**__${message.guild.memberCount}__**]`)
+        .addField(`:first_place:  **__Roles Count:__**`, `**➥** [**__${message.guild.roles.size}__**]`)
+        .addField(`:date: **__Server Created At:__**`, `**➥** [__${message.guild.createdAt.toLocaleString()}__]`)
+        .addField(`:green_heart: **__Online Members__**`, `[__${message.guild.members.filter(m=>m.presence.status == 'online').size}__]`)
+        .addField(`:yellow_heart: **__Idle Members__**`, `[__${message.guild.members.filter(m=>m.presence.status == 'idle').size}__]`)
+        .addField(`:heart: **__Dnd Members__**`, `[__${message.guild.members.filter(m=>m.presence.status == 'dnd').size}__]`)
+        .addField(`:black_circle: **__Offline Members__**`, `[__${message.guild.members.filter(m=>m.presence.status == 'offline').size}__]`)
+		.addField( `:loud_sound: **__Voice Roms__**`,`[__${message.guild.channels.filter(m => m.type === 'voice').size}__]`)
+        .setColor('GRAY')
+        .setThumbnail(client.user.avatarURL)
+        message.channel.sendEmbed(server)
     }
 });
 
 
-
- 
 client.login(process.env.BOT_TOKEN);
