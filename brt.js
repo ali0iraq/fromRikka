555bb6ed890ce712a2cd06ed8fe3cbadbc9df541
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-28 14:23:47 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -43,29 +43,8 @@ client.on('message', message => {
     }
 });
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
-  let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = msg.content.split(" ").slice(1);
 
-    if(command === "clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-        msg.delete().then
-    msg.channel.send("***```50 👌```***").then(m => m.delete(3000));
-} else {
-    msg.delete().then
-    msg.delete().then
-    msg.channel.bulkDelete(textxt);
-        msg.channel.send("```php\n Number of messages that have been cleared: " + textxt + "\n```").then(m => m.delete(3000));
-        }    
-    }
-}
-});
+
 ////////////////////////////////////////
 
 client.login(process.env.BOT_TOKEN);
