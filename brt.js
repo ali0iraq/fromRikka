9cc0cc47bfad95d5a123ce1c2971f3b2305bbd78
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-30 02:19:16 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -81,13 +81,46 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.author.bot) return;
+    if (const child_process = require("child_process");
+const devs = ['396958215377780747'];
+
+client.on('message', message => {
+if(message.content === prefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+        client.destroy();
+        child_process.fork(__dirname + "/الملف.js");
+        console.log(تم اعادة تشغيل البوت);
+    }
+  
+  });.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
     if (message.content.startsWith(prefix + 'ping')) {
         message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
     }
 });
+
+
+
+const child_process = require("child_process");
+const devs = ['396958215377780747'];
+
+client.on('message', message => {
+if(message.content === prefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+        client.destroy();
+        child_process.fork(__dirname + "/الملف.js");
+        console.log(تم اعادة تشغيل البوت);
+    }
+  
+  });
+
+
+
 
 ////////////////////////////////////////
 
