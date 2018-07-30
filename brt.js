01bf82d9352e785843fdbfe995e84832943c2eef
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-30 14:37:05 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -80,17 +80,6 @@ client.on('message', message => {
 
 });
 
-  
-  });.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    if (message.content.startsWith(prefix + 'ping')) {
-        message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-    }
-});
-
-
-
 ////////////////////////////////////////
 
 client.login(process.env.BOT_TOKEN);
