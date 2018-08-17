54ad0c4d299c86d2c7b6bc8bc3f5cb4b04728708
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-17 13:40:46 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -1,6 +1,10 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "+";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 
 client.on('message', message => {
    if(!message.channel.guild) return;
