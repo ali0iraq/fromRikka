44f9ff47ebf2f7a9f0570a44e8353d15b5239941
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-17 15:16:14 +0300

    Update brt.js

--- brt.js
+++ brt.js
@@ -14,9 +14,9 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.s
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let BcList = new Discord.RichEmbed()
 .setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله ${args}`)
-.setDescription(`لديك دقيقه للأختيار قبل الغاء البرودكاست`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+.setAuthor(`Message content ${args}`)
+.setDescription(`You have a minute to choose before you cancel a message`)
+if (!args) return message.reply('**Please write the text you want to send**');message.channel.send(BcList).then(msg => {
 msg.react('📝')
 .then(() => msg.react('✏'))
 .then(() =>msg.react('📝'))
@@ -28,7 +28,7 @@ let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
 let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
  
 EmbedBc.on("collect", r => {
-message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+message.channel.send(`:ballot_box_with_check: Message sent successfully`).then(m => m.delete(5000));
 message.guild.members.forEach(m => {
 var bc = new
 Discord.RichEmbed()
@@ -42,7 +42,7 @@ msg.delete();
 })
 })
 NormalBc.on("collect", r => {
-  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+  message.channel.send(`:ballot_box_with_check: Message sent successfully`).then(m => m.delete(5000));
 message.guild.members.forEach(m => {
 m.send(args);
 msg.delete();
